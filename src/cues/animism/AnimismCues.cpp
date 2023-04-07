91e82489c5127794647dac9774b627d437e6424b
ramagottfried <rama.gottfried@gmail.com>
2023-04-07 11:42:12 +0200

    cleaning up folder structure, moved shaders to folder, moved anim files to separate folder

--- src/cues/animism/AnimismCues.cpp
+++
@@ -1,1 +0,0 @@
-#include "AnimismCues.hpp"
--- src/cues/animism/cue_nothing.cpp
+++
@@ -1,9 +0,0 @@
-#include "AnimismCues.hpp"
-
-using namespace cvgl;
-using namespace Eigen;
-
-MapOSC cue_nothing(cueArgs args)
-{
-    return MapOSC();
-}
